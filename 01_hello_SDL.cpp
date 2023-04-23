5708edbbcc6465e709323f0efe6f71ee0a71a3fe
Kaze Phake <phongsgvn@gmail.com>
2023-04-23 16:34:57 +0700

    Updated syntax

--- 01_hello_SDL.cpp
+++ 01_hello_SDL.cpp
@@ -6,17 +6,15 @@ const int SCREEN_HEIGHT = 480;
 
 int main( int argc, char* args[] )
 {
-	SDL_Window* window = NULL;
-	
+	SDL_Window* window = NULL;	
 	SDL_Surface* screenSurface = NULL;
-
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 	{
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
 	}
 	else
 	{
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		window = SDL_CreateWindow( "Kaze Phake", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 		if( window == NULL )
 		{
 			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
@@ -24,18 +22,12 @@ int main( int argc, char* args[] )
 		else
 		{
 			screenSurface = SDL_GetWindowSurface( window );
-
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0x00, 0x00, 0x00 ) );
-			
-			SDL_UpdateWindowSurface( window );
-            
+			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0x00, 0xFF ) );			
+			SDL_UpdateWindowSurface( window );            
             SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
 		}
 	}
-
 	SDL_DestroyWindow( window );
-
 	SDL_Quit();
-
 	return 0;
 }
